4e7e659be2eb1a57118fc5d7384386495abb782f
Emily Butler <scejb@leeds.ac.uk>
2021-03-15 11:02:59 +0000

    Add cos graph

---
+++ src/cos.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = np.arange(0,10,0.1)
+amplitude = np.cos(time)
+
+plt.plot(time,amplitude)
+plt.title("Cosine Curve")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+
+plt.show()
