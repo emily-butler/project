b27741d4bb522e2c40388e32c66654a51b995618
Emily Butler <scejb@leeds.ac.uk>
2021-03-15 11:03:10 +0000

    Add sine graph

---
+++ src/sin.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = np.arange(0,10,0.1)
+amplitude = np.sin(time)
+
+plt.plot(time,amplitude)
+plt.title("Sine Curve")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+
+plt.show()
